8aad16928bfe8be18ba9c04515c4350d521e9cc5
orange-beard <97.aksakal@gmail.com>
2021-02-05 08:42:47 +0300

    Developed box things

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app.permanent_session_lifetime = timedelta(minutes=5)
 
 @app.route('/')
 def main():
-    return "anasayfa"
+    return redirect('login')
 
 
 @app.route('/register/', methods=['GET','POST'])
@@ -67,7 +67,7 @@ def login():
         try:
             con = sqlite3.connect('database.db')
             cur = con.cursor()
-            cur.execute("SELECT mail, name, surname, type FROM users WHERE mail = ? and pwd = ?", (mail, pwd,))
+            cur.execute("SELECT id, mail, name, surname, type FROM users WHERE mail = ? and pwd = ?", (mail, pwd,))
             res = cur.fetchall()
         except:
             return "Veritabanı hatası"
@@ -87,14 +87,101 @@ def logout():
     return redirect('/')
 
 
+@app.route('/admin/', methods=['GET', 'POST'])
+def admin():
+    if "user" not in session:
+        return redirect('/login/')
+    if session["user"][4] == 1:
+        if request.method == 'POST':
+            box_code = request.form['box_code'].lower()
+            capacity = request.form['capacity']
+            timestamp = str(time.time()+10800).split('.')[0]
+            con = sqlite3.connect('database.db')
+            cur = con.cursor()
+            try:
+                cur.execute("SELECT id FROM boxes WHERE box_code = ?", (box_code,))
+                res = cur.fetchall()
+            except:
+                return "Veritabanı Hatası1"
+            if res != []:
+                return "Kutu Zaten Mevcut!"
+            try:
+                cur.execute("INSERT INTO boxes (box_code, max_limit, date) VALUES (?, ?, ?)", (box_code, capacity, timestamp))
+                con.commit()
+            except:
+                return "Veritabanı Hatası"
+            finally:
+                con.close()
+            return "Kutu Eklendi"
+        elif request.method == 'GET':
+            return render_template('admin.html')
+    else:
+        return redirect('/box/')
+
+
 @app.route('/box/', methods=['POST', 'GET'])
 def box():
     if "user" not in session:
         return redirect('/login/')
-    if request.method == 'POST':
-        return render_template("box.html")
-    return render_template("box.html")
+    con = sqlite3.connect('database.db')
+    cur = con.cursor()
+    try:
+        cur.execute("SELECT id, box_code FROM boxes WHERE user_id = ?", (session["user"][0],))
+        boxes = cur.fetchall()
+    except:
+        return "Veritabanı hatası"
+    box_settings = []
+    try:
+        for box in boxes:
+            cur.execute("SELECT name, expiration, trig_limit FROM box_settings WHERE box_id = ?", (box[0],))
+            res = cur.fetchall()
+            res = list(res[0])
+            res.insert(0, box[1])
+            box_settings.append(res)
+    except:
+        return "Veritabanı hatası"
+    finally:
+        con.close()
     
+    return render_template('box.html', bs = box_settings)
+    
+@app.route('/box/<path>/', methods=['POST', 'GET'])
+def crud_box(path):
+    if request.method == 'POST':
+        if path.lower() == 'create':
+            box_code = request.form['box_code'].lower()
+            con = sqlite3.connect('database.db')
+            cur = con.cursor()
+            try:
+                cur.execute("SELECT id, user_id FROM boxes WHERE box_code = ?", (box_code,))
+                res = cur.fetchall()
+            except:
+                return "Veritabanı hatası"
+            if res == []:
+                return "Kutu Mevcut Değil!"
+            elif res[0][1] != None:
+                return "Kutu Zaten Tanımlı"
+            try:
+                cur.execute("UPDATE boxes SET user_id = ? WHERE id = ?", (session["user"][0], res[0][0],))
+                con.commit()
+            except:
+                return "Update hata"
+            try:
+                timestamp = str(time.time()+10800).split('.')[0]
+                cur.execute("INSERT INTO box_settings (box_id, name, expiration, trig_limit, date) VALUES (?, ?, ?, ?, ?)", (res[0][0], "Belirtilmemiş", timestamp, 0, timestamp))
+                con.commit()
+            except:
+                return "Box Settings Hata"
+            finally:
+                con.close()
+
+            return "create"
+        elif path.lower() == 'update':
+            return "up"
+        else:
+            return "404"
+    else:
+        return "get "+path
 
 if __name__ == "__main__":
     app.run(debug=True)
